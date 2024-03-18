9f4a9da32a2cadd2b18903dacc850674c9fd091c
NNakreSS <serkanatmaca15@gmail.com>
2024-03-18 19:39:02 +0300

    getAllUsers

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 import asyncErrorWrapper from "express-async-handler";
+import User from "../models/User.js";
 
 const getUserById = asyncErrorWrapper(async (req, res, next) => {
   const user = req.data;
@@ -9,4 +10,13 @@ const getUserById = asyncErrorWrapper(async (req, res, next) => {
   });
 });
 
-export { getUserById };
+const getAllUsers = asyncErrorWrapper(async (req, res, next) => {
+  const users = await User.find();
+
+  return res.status(200).json({
+    success: true,
+    data: users,
+  });
+});
+
+export { getUserById, getAllUsers };
--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getUserById } from "../controllers/user.js";
+import { getUserById, getAllUsers } from "../controllers/user.js";
 // middlewares
 import { checkUserExist } from "../middlewares/database/dbErrorHelpers.js";
 
 const router = Router();
 
 //? get methods
+router.get("/", getAllUsers);
 router.get("/:id", checkUserExist, getUserById);
 
 export default router;
