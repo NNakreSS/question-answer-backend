799269fe65154c2544f7a1ec29994f068a68d23d
NNakreSS <serkanatmaca15@gmail.com>
2024-03-19 00:14:41 +0300

    add question methods

--- controllers/question.js
+++ controllers/question.js
@@ -1,3 +1,7 @@
+import CustomError from "../helpers/errors/CustomError.js";
+import Question from "../models/Question.js";
+import asyncErrorWrapper from "express-async-handler";
+
 const getAllQuestions = (req, res, next) => {
   res.status(200).json({
     success: true,
@@ -5,4 +9,18 @@ const getAllQuestions = (req, res, next) => {
   });
 };
 
-export { getAllQuestions };
+const askNewQuestion = asyncErrorWrapper(async (req, res, next) => {
+  const information = req.body;
+
+  const question = await Question.create({
+    ...information,
+    author: req.user.id,
+  });
+
+  return res.status(200).json({
+    success: true,
+    data: question,
+  });
+});
+
+export { getAllQuestions, askNewQuestion };
--- routers/questions.js
+++ routers/questions.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { getAccessToRoute } from "../middlewares/authorization/auth.js";
+import { askNewQuestion } from "../controllers/question.js";
+
 const router = express.Router();
 
 //? controllers
@@ -6,9 +9,11 @@ import { getAllQuestions } from "../controllers/question.js";
 
 //? get methods
 router.get("/", getAllQuestions);
-
 router.get("/delete", (req, res) => {
   res.send("<h1>Questions Delete Page</h1>");
 });
+
+//? post methods
+router.post("/ask", getAccessToRoute, askNewQuestion);
 
 export default router;
