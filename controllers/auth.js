9a3908c0aa75aa806ec35344bbbdb042a0e52f9c
NNakreSS <serkanatmaca15@gmail.com>
2024-03-18 17:59:47 +0300

    send mail

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
 import User from "../models/User.js";
 // async error wrapper
 import asyncErrorWrapper from "express-async-handler";
-// helpoers authorization
+// helpers
 import { sendJwtToClient } from "../helpers/authorization/tokenHelpers.js";
 import validateUserInput from "../helpers/input/inputHelpers.js";
 import CustomError from "../helpers/errors/CustomError.js";
 import comparePassword from "../helpers/authorization/comparePassword.js";
+import sendEmail from "../helpers/libraries/sendEmail.js";
 
 const register = asyncErrorWrapper(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -91,10 +92,32 @@ const forgotpassword = asyncErrorWrapper(async (req, res, next) => {
 
   await user.save();
 
-  res.json({
-    success: true,
-    message: "Password reset link sent to your email",
-  });
+  const resetPasswordUrl = `https://localhost:5000/api/auth/resetpassword?reset_password_token=${resetPassWordToken}`;
+
+  const emailTemplate = `
+    <h3>Reset Your Password</h3>
+    <p> This <a href="${resetPasswordUrl}" target="_blank">link</a> will expire in 1 hour</p>
+  `;
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_USER,
+      to: resetEmail,
+      subject: "Reset Your Password",
+      html: emailTemplate,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Password reset link sent to your email",
+    });
+  } catch (err) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+
+    return next(new CustomError("Email could Not be Sent", 500));
+  }
 });
 
 export {
