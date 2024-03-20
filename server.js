0343ce40c20eb34323678af64ee87ec999383e32
NNakreSS <serkanatmaca15@gmail.com>
2024-03-20 21:33:24 +0300

    create routers preview

--- server.js
+++ server.js
@@ -25,24 +25,22 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-//? default url get
-app.get("/", (req, res) => {
-  //get all routes
-  const routes = listEndpoints(app);
-  // create HTML
-  let html =
-    "<html><head><style>table { border-collapse: collapse; width: 100%; } th, td { padding: 8px; text-align: left; border-bottom: 1px solid #ddd; } th { background-color: #f2f2f2; } .get { background-color: #c0e4ff; } .post { background-color: #cff4d2; } .put { background-color: #ffefc0; } .delete { background-color: #ffc0cb; }</style></head><body>";
-  html += "<h1>API Routs</h1>";
-  html += "<table><tr><th>Metod</th><th>Rota</th></tr>";
-  routes.forEach((route) => {
-    const method = route.methods.join(", ").toUpperCase();
-    const colorClass = method.toLowerCase();
-    html += `<tr class="${colorClass}"><td>${method}</td><td>${route.path}</td></tr>`;
-  });
-  html += "</table></body></html>";
+//? views
+app.set("views", path.join(process.cwd(), "views"));
+// ejs engine
+app.set("view engine", "ejs");
 
-  // send client to HTML
-  res.send(html);
+//? default url get - view routes
+app.get("/", (req, res) => {
+  const allroutes = listEndpoints(app);
+  const routes = {};
+  allroutes.forEach((route) => {
+    routes[route.methods[0]] = routes[route.methods[0]]
+      ? [...routes[route.methods[0]], route.path]
+      : [route.path];
+  });
+  console.log(routes);
+  res.render("routes", { routes });
 });
 
 //? api routers middleware
